efd8539a9b5993b0a2724ceeea4ae1861ef794da
martin94p <martin94p@gmail.com>
2023-02-13 13:24:06 -0300

    Implementacion de estilo v2
    Darle un poco mas de buena imagen ux
    Se soluciona algunos errores de implementacion de tailwind

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -15,35 +15,46 @@ function EmployeesList() {
         
     }
 
-   
+    
+
    return (
-    <div class='w-4/6'>
-        <header className='flex justify-'> 
+    
+    <div class='w-4/6' display="flex "justifyContent='center'>
         <h1><c>Empleados Distinguidos!</c></h1>
+        <header className='flex justify-center'> 
+        <h2>
         <Link to="/create-employee" 
         className="bg-indigo-600 px-2 py-1 rounded-sm text-sm"
         >
         Crear Nuevo Empleado
-        </Link>
+        </Link></h2>
         </header>
         
         <div className="grid grid-cols-3 gap-5">
         {employees.map(employee => (
             <div key={employee.id} className="bg-neutral-400 p-5 rounded-md">
+                <div >
                 <header className="flex justify-between">
-                <p>{employee.First_Name}</p>
+                <h4>{employee.First_Name}</h4>
+                </header>    
+                
                 <p>{employee.Last_Name}</p>
                 <p>{employee.Email}</p>
                 <p>{employee.Hire_Date}</p>
                 <p>{employee.Salary}</p>
                 <p>{employee.Commission_PCT}</p>
-                <div className="flex">
-                <button onClick={() => handleDelete(employee.id)}>Eliminar</button>
-                <Link to={`/edit-employee/${employee.id}`}>Editar</Link>       
-                </div>
-                </header>
+                <button onClick={() => handleDelete(employee.id)}
+                        class="bg-red-500 px-2 py-1 text-xs rounded-md self-center"
+                        >Eliminar
+                </button>
+                <Link to={`/edit-employee/${employee.id}`}
+                className="bg-zinc-600 px-2 py-1 text-xs rounded-md"
+                >Editar</Link>       
+                 
+                
+                 </div>       
             </div>
-        ))}
+        ))};
         </div>            
     </div>
   )
